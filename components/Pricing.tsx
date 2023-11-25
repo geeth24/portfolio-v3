6f09126a5463c01d3d13d1538607250b85a532b7
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-11-25 03:01:58 -0600

    Update price in Starter Package

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -8,7 +8,7 @@ const tiers = [
     href: '#',
     description: 'The Starter Package',
     features: ['Logo Design', 'Landing Page Design'],
-    price: 1000,
+    price: 1500,
   },
   {
     name: 'Custom',
